11828080992270b7ad44b4e41b5b6962d10fd8dc
parica <parisanaderi456@gmail.com>
2022-11-22 18:24:12 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let countEl = document.getElementById("counter")
+
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+function save() {
+    console.log(count)
+}
+save()
